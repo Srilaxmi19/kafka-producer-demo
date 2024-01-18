5fd21de90c5683d4be3a941943ff2a573b938c7e
Srilaxmi19 <srilaxmi19@gmail.com>
2024-01-17 16:20:14 -0800

    Serialization & Deserialization config added

--- src/main/java/com/olive/controller/EventController.java
+++ src/main/java/com/olive/controller/EventController.java
@@ -1,13 +1,11 @@
 package com.olive.controller;
 
+import com.olive.dto.Customer;
 import com.olive.service.KafkaMessagePublisher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/producer")
@@ -19,7 +17,7 @@ public class EventController {
     @GetMapping("/publish/{message}")
     public ResponseEntity<?> publishMessage(@PathVariable String message) {
         try {
-            for (int i = 0; i <= 1000; i++) {
+            for (int i = 0; i <= 100000; i++) {
                 publisher.sendMessageToTopic(message + " : " + i);
             }
             return ResponseEntity.ok("Message published successfully ..");
@@ -27,5 +25,10 @@ public class EventController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .build();
         }
+    }
+
+    @PostMapping("/publish")
+    public void sendEvents(@RequestBody Customer customer) {
+        publisher.sendEventsToTopic(customer);
     }
 }
--- src/main/java/com/olive/service/KafkaMessagePublisher.java
+++ src/main/java/com/olive/service/KafkaMessagePublisher.java
@@ -1,5 +1,6 @@
 package com.olive.service;
 
+import com.olive.dto.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
@@ -15,7 +16,7 @@ public class KafkaMessagePublisher {
 
 
     public void sendMessageToTopic(String message){
-        CompletableFuture<SendResult<String, Object>> future = template.send("kafka-demo-4", message);
+        CompletableFuture<SendResult<String, Object>> future = template.send("kafka-test-object", message);
         future.whenComplete((result,ex)->{
             if (ex == null) {
                 System.out.println("Sent message=[" + message +
@@ -26,5 +27,23 @@ public class KafkaMessagePublisher {
             }
         });
 
+    }
+
+    public void sendEventsToTopic(Customer customer) {
+        try {
+            CompletableFuture<SendResult<String, Object>> future = template.send("kafka-test-object", customer);
+            future.whenComplete((result, ex) -> {
+                if (ex == null) {
+                    System.out.println("Sent message=[" + customer.toString() +
+                            "] with offset=[" + result.getRecordMetadata().offset() + "]");
+                } else {
+                    System.out.println("Unable to send message=[" +
+                            customer.toString() + "] due to : " + ex.getMessage());
+                }
+            });
+
+        } catch (Exception ex) {
+            System.out.println("ERROR : "+ ex.getMessage());
+        }
     }
 }
